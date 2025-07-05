ae2f92f909d15e8f483acf6d4bef73b24f434267
Nicolas HORNUEL <nicolas.hornuel@gmail.com>
2025-07-05 09:31:58 +0200

    wilcard cross origin

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const youtubeRouter = require('./app/routers/youtube.router');
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:4200', 'https://agenda-bf245.web.app'];
-
+// Liste des origines autorisées
+/* const allowedOrigins = ['http://localhost:4200', 'https://agenda-bf245.web.app'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -17,13 +17,15 @@ const corsOptions = {
   },
   credentials: true,
 };
+app.use(cors(corsOptions)); */
+
+//ajout de CORS pour permettre à l'application front en local de consomme l'API
+app.use(cors());
 
 const headers = {
   'Access-Control-Allow-Headers': 'x-access-token, Origin, Content-Type, Accept'
 };
 
-//ajout de CORS pour permettre à l'application front en local de consomme l'API
-app.use(cors(corsOptions));
 app.use(function (req, res, next) {
   res.header(headers);
   next();
